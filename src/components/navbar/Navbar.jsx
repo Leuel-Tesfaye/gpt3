83097c15f42624d10e5bcd2be3110e36e902e2c1
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-11-21 09:45:30 +0300

    assets file change and navbar component change

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./Navbar.css";
+// BEM -> Block Element Modifier
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="gpt3__navbar">
+        <div className="gpt3__navbar-links">
+          <div className="gpt3__navbar-links-logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="gpt3__navbar-links-container">
+            <p>
+              <a href="#home">Home</a>
+            </p>
+            <p>
+              <a href="#whatGpt3">What Is Gpt3</a>
+            </p>
+            <p>
+              <a href="#possibility">Open AI</a>
+            </p>
+            <p>
+              <a href="#features">Case Studies</a>
+            </p>
+            <p>
+              <a href="#blog">Library</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
