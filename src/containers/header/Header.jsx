d386b30bcd4838b97418877f08ff78c3030fb68c
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-11-27 08:59:32 +0300

    header changes made committed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -24,9 +24,9 @@ const Header = () => {
           <img src={peopleImage} alt="People " />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        {/* <div className="gpt3__header-image">
-          <img src={ai} alt="ai person on header" />
-        </div> */}
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai person on header" />
       </div>
     </div>
   );
